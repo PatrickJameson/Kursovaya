06c9cc053cd86b966892ab3dacb81bfcf45c7dfb
Гуренко Дмитрий <55867811+PatrickJameson@users.noreply.github.com>
2020-04-19 19:47:48 +0500

    Update 24.py

--- 2 часть курсовой/24.py
+++ 2 часть курсовой/24.py
@@ -2,14 +2,20 @@ import numpy as np
 
 N = 4
 M = 5
+L = np.random.randint(1, 3)
+K = np.random.randint(1, 3)
 
 A = np.random.randint(low=-9, high=10, size=(N, M))
 print("Матрица:\r\n{}\n".format(A))
-
-iu = np.triu_indices(N, 1)
-a = A[iu]
-a = np.sum(np.array(a))
-print("\nCумма элементов выше главной диагонали = " + str(a))
-b = np.fliplr(A)[iu]
-b = np.prod(np.array(b))
-print("\nПроизведение элементов выше побочной диагонали = " + str(b))
+x = A[0:L ,0:K]
+x_sum = x.sum()
+print("Вверхняя левая часть: сумма равна = " + str(x_sum) + "\n" + str(x))
+y = A[L: ,0 :K]
+y_sum = y.sum()
+print("\nНижняя левая часть: сумма равна = " + str(y_sum) + "\n" + str(y))
+z = A[0:L ,K:]
+z_sum = z.sum()
+print("\nВверхняя правая часть: сумма равна = " + str(z_sum) + "\n" + str(z))
+a = A[L: ,K:]
+a_sum = a.sum()
+print("\nНижняя правая часть: сумма равна = " + str(a_sum) + "\n" + str(a))
